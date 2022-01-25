d666ea0c3630b9e7a979690077a7092c0d5ee14b
visitnoufal <visitnoufal@gmail.com>
2022-01-25 20:28:18 +0530

    Update DetailScreen.js

--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -5,27 +5,33 @@ import Languages from "../components/Languages";
 import React from "react";
 import { ScrollView } from "react-native";
 
+/**
+ * Detail Screen
+ */
 function DetailScreen({ navigation, route }) {
   const data = route.params?.data || {};
+
+  /**
+   * Navigate to map screen
+   * Passing latitude, longitude and area
+   */
+  function onViewMap() {
+    navigation.navigate("Map", {
+      latlng: data.latlng,
+      area: data.area,
+      title: data.name,
+    });
+  }
+
   return (
     <ScrollView>
       <Card>
-        <Card.Cover source={{ uri: data.flags.png }} />
+        <Card.Cover source={{ uri: data.flag }} />
         <Card.Title title={data.name} subtitle={data.nativeName} />
       </Card>
       <Description data={data} />
       <Card style={{ marginHorizontal: 10 }}>
-        <Button
-          mode="contained"
-          uppercase={false}
-          onPress={() =>
-            navigation.navigate("Map", {
-              latlng: data.latlng,
-              area: data.area,
-              title: data.name,
-            })
-          }
-        >
+        <Button mode="contained" uppercase={false} onPress={onViewMap}>
           View country map
         </Button>
       </Card>
